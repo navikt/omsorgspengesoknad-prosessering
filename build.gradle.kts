6779520d3895e855cfd212388706b84f2e8304b4
Erik Maximilian Forsman <Erik.Maximilian.Forsman@nav.no>
2020-03-23 13:25:09 +0100

    Kafka update

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,8 +8,8 @@ val slf4jVersion = ext.get("slf4jVersion").toString()
 val kotlinxCoroutinesVersion = ext.get("kotlinxCoroutinesVersion").toString()
 
 val openhtmltopdfVersion = "1.0.2"
-val kafkaEmbeddedEnvVersion = "2.2.3"
-val kafkaVersion = "2.3.0" // Alligned med version fra kafka-embedded-env
+val kafkaEmbeddedEnvVersion = "2.4.0"
+val kafkaVersion = "2.4.0" // Alligned med version fra kafka-embedded-env
 val handlebarsVersion = "4.1.2"
 
 val mainClass = "no.nav.helse.OmsorgspengesoknadProsesseringKt"
