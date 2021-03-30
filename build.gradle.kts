b34b23ff0797c251785f8feb22d4d3a7ba225788
Sander Opperud Odden <sander.opperud.odden@nav.no>
2021-03-30 11:17:21 +0200

    Trekker ut kafka versjoner fra dusseldorfktor og laster ned "https://jitpack.io"

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,8 +8,8 @@ val slf4jVersion = ext.get("slf4jVersion").toString()
 val kotlinxCoroutinesVersion = ext.get("kotlinxCoroutinesVersion").toString()
 
 val openhtmltopdfVersion = "1.0.8"
-val kafkaEmbeddedEnvVersion = "2.4.0"
-val kafkaVersion = "2.4.0" // Alligned med version fra kafka-embedded-env
+val kafkaEmbeddedEnvVersion = ext.get("kafkaEmbeddedEnvVersion").toString()
+val kafkaVersion = ext.get("kafkaVersion").toString() // Alligned med version fra kafka-embedded-env
 val handlebarsVersion = "4.1.2"
 
 val mainClass = "no.nav.helse.OmsorgspengesoknadProsesseringKt"
@@ -74,6 +74,7 @@ repositories {
     }
 
     mavenCentral()
+    maven("https://jitpack.io")
     jcenter()
 
     maven("https://dl.bintray.com/kotlin/ktor")
