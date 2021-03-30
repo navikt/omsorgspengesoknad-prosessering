783e80e97a9e18dff449c792e24259ecbba5eebf
SanderOpperud <60642441+SanderOpperud@users.noreply.github.com>
2021-03-30 11:34:48 +0200

    Oppdaterer dusseldorfktor, kotlinjvm og openhtmltopdfVersion (#61)
    * Oppdaterer dusseldorfktor, kotlinjvm og openhtmltopdfVersion
    
    * Trekker ut kafka versjoner fra dusseldorfktor og laster ned "https://jitpack.io"
    
    * Fjerner utgått jcenter

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,27 +1,27 @@
 import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
-val dusseldorfKtorVersion = "1.5.1.609bb61"
+val dusseldorfKtorVersion = "1.5.2.5b2dff7"
 val k9FormatVersion = "5.1.16"
 val ktorVersion = ext.get("ktorVersion").toString()
 val slf4jVersion = ext.get("slf4jVersion").toString()
 val kotlinxCoroutinesVersion = ext.get("kotlinxCoroutinesVersion").toString()
 
-val openhtmltopdfVersion = "1.0.6"
-val kafkaEmbeddedEnvVersion = "2.4.0"
-val kafkaVersion = "2.4.0" // Alligned med version fra kafka-embedded-env
+val openhtmltopdfVersion = "1.0.8"
+val kafkaEmbeddedEnvVersion = ext.get("kafkaEmbeddedEnvVersion").toString()
+val kafkaVersion = ext.get("kafkaVersion").toString() // Alligned med version fra kafka-embedded-env
 val handlebarsVersion = "4.1.2"
 
 val mainClass = "no.nav.helse.OmsorgspengesoknadProsesseringKt"
 
 plugins {
-    kotlin("jvm") version "1.4.31"
+    kotlin("jvm") version "1.4.32"
     id("com.github.johnrengelman.shadow") version "6.1.0"
 }
 
 buildscript {
     // Henter ut diverse dependency versjoner, i.e. ktorVersion.
-    apply("https://raw.githubusercontent.com/navikt/dusseldorf-ktor/609bb6132fa5a3d25cc3816f0e53f656d24e1549/gradle/dusseldorf-ktor.gradle.kts")
+    apply("https://raw.githubusercontent.com/navikt/dusseldorf-ktor/5b2dff7a11531e487ebdee06b6ae94e0e50287c2/gradle/dusseldorf-ktor.gradle.kts")
 }
 
 dependencies {
@@ -74,7 +74,7 @@ repositories {
     }
 
     mavenCentral()
-    jcenter()
+    maven("https://jitpack.io")
 
     maven("https://dl.bintray.com/kotlin/ktor")
     maven("https://kotlin.bintray.com/kotlinx")
