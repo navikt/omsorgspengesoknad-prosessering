3e7febc29844707f5fb5a671bbb825da12d0c919
Sander Opperud Odden <sander.opperud.odden@nav.no>
2021-07-05 10:00:28 +0200

    Fjerner logging av generering av pdf

--- src/main/kotlin/no/nav/helse/prosessering/v1/PdfV1Generator.kt
+++ src/main/kotlin/no/nav/helse/prosessering/v1/PdfV1Generator.kt
@@ -7,12 +7,14 @@ import com.github.jknack.handlebars.context.MapValueResolver
 import com.github.jknack.handlebars.io.ClassPathTemplateLoader
 import com.openhtmltopdf.outputdevice.helper.BaseRendererBuilder
 import com.openhtmltopdf.pdfboxout.PdfRendererBuilder
+import com.openhtmltopdf.util.XRLog
 import no.nav.helse.aktoer.NorskIdent
 import no.nav.helse.dusseldorf.ktor.core.fromResources
 import java.io.ByteArrayInputStream
 import java.io.ByteArrayOutputStream
 import java.time.ZoneId
 import java.time.format.DateTimeFormatter
+import java.util.logging.Level
 
 internal class PdfV1Generator {
     private companion object {
@@ -59,6 +61,7 @@ internal class PdfV1Generator {
         barnetsIdent: NorskIdent?,
         barnetsNavn: String?
     ): ByteArray {
+        XRLog.listRegisteredLoggers().forEach { logger -> XRLog.setLevel(logger, Level.WARNING) }
         soknadTemplate.apply(
             Context
                 .newBuilder(
