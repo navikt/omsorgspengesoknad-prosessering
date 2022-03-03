4f8a8bcb2ff0e61762bc2bc6dfb99c25800e3eff
SanderOpperud <60642441+SanderOpperud@users.noreply.github.com>
2022-03-03 15:52:57 +0100

    Oppdaterer pakker. (#135)
    * Closes #134
    * Closes #133
    * Closes #132
    * Closes #120
    * Closes #119

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
-val dusseldorfKtorVersion = "3.1.6.7-7d29e37"
-val k9FormatVersion = "5.7.2"
+val dusseldorfKtorVersion = "3.1.6.7-f2a96e8"
+val k9FormatVersion = "5.8"
 val ktorVersion = ext.get("ktorVersion").toString()
 val slf4jVersion = ext.get("slf4jVersion").toString()
 val kotlinxCoroutinesVersion = ext.get("kotlinxCoroutinesVersion").toString()
@@ -17,12 +17,12 @@ val mainClass = "no.nav.helse.OmsorgspengesoknadProsesseringKt"
 
 plugins {
     kotlin("jvm") version "1.6.10"
-    id("com.github.johnrengelman.shadow") version "7.1.1"
+    id("com.github.johnrengelman.shadow") version "7.1.2"
 }
 
 buildscript {
     // Henter ut diverse dependency versjoner, i.e. ktorVersion.
-    apply("https://raw.githubusercontent.com/navikt/dusseldorf-ktor/7d29e37518df576f6503f5a2e5e1bac0366d2957/gradle/dusseldorf-ktor.gradle.kts")
+    apply("https://raw.githubusercontent.com/navikt/dusseldorf-ktor/f2a96e854a9b6dbd8d9b010a6860c63d7ca8a48c/gradle/dusseldorf-ktor.gradle.kts")
 }
 
 dependencies {
