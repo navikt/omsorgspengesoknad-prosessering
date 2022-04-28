18ba3944fc4f0b0d614dbd4536fc50f1916a1716
SanderOpperud <60642441+SanderOpperud@users.noreply.github.com>
2022-04-28 15:00:59 +0200

    Oppdatere til java 17 (#153)

--- build.gradle.kts
+++ build.gradle.kts
@@ -87,13 +87,13 @@ repositories {
 
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_11
-    targetCompatibility = JavaVersion.VERSION_11
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
 }
 
 
 tasks.withType<KotlinCompile> {
-    kotlinOptions.jvmTarget = "11"
+    kotlinOptions.jvmTarget = "17"
 }
 
 tasks.withType<ShadowJar> {
