4f96cba9b5bb23283de222f4e9df7785e932140c
Ramin Esfandiari <ramin_esfandiari_93@hotmail.com>
2022-06-20 13:34:05 +0200

    Samler metrics under et object.

--- src/main/kotlin/no/nav/helse/OmsorgspengesoknadProsessering.kt
+++ src/main/kotlin/no/nav/helse/OmsorgspengesoknadProsessering.kt
@@ -3,7 +3,6 @@ package no.nav.helse
 import com.fasterxml.jackson.databind.PropertyNamingStrategies
 import com.fasterxml.jackson.databind.SerializationFeature
 import io.ktor.server.application.*
-import io.ktor.server.plugins.*
 import io.ktor.http.*
 import io.ktor.serialization.jackson.*
 import io.ktor.server.plugins.contentnegotiation.*
